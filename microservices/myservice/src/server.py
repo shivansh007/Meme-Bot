9a7e7f637c9c24150fcc211f622eb327c3768d1b
Shivansh Nalwaya <shivansh@Shivanshs-MacBook-Air.local>
2018-01-19 00:28:29 +0530

    IC

--- microservices/myservice/src/server.py
+++ microservices/myservice/src/server.py
@@ -10,12 +10,13 @@ def index():
 
 @app.route("/webhook", methods = ['GET', 'POST'])
 def webhook():
+	print(jsonify(request.args))
 	verify_token = request.args.get('hub.verify_token')
 	challenge = request.args.get('hub.challenge')
 	if verify_token == FACEBOOK_VERIFY_TOKEN:
 		return challenge
 	else:
-		print(jsonify(request.args))
+		return jsonify(res = request.args)
 
 # @app.route("/webhook", methods = ['POST'])
 # def message():
