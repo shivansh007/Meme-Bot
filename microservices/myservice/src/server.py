ece6fdce060da22d15430a2f7db58f964630c09e
Shivansh Nalwaya <shivansh@Shivanshs-MacBook-Air.local>
2018-01-19 16:19:12 +0530

    IC

--- microservices/myservice/src/server.py
+++ microservices/myservice/src/server.py
@@ -101,7 +101,7 @@ def send_image(sid, search):
 
 def get_image(search):
 		url = "https://api.imgur.com/3/gallery/search/?q='" + search +" memes'"
-		res = requests.request(url = url, method = 'GET', headers = { "Authorizaion": "Client-ID e21842678284d02", "Content-Type": "application/json" })
+		res = requests.request(url = url, method = 'GET', headers = { "Authorization": "Client-ID e21842678284d02", "Content-Type": "application/json" })
 		# print(url, res.json())
 		if 'error' in res.json()["data"]:
 			return "Not Found"
